5330cf9e8b2a3c2facc046dfd9e94d5d879ed442
Niu Kun <niukun@corp.netease.com>
2015-07-11 14:00:29 +0800

    add page vc

---
+++ MNImagePicker/MNAssetPageViewController.h
@@ -0,0 +1,16 @@
+//
+//  MNAssetPageViewController.h
+//  MNImagePicker
+//
+//  Created by Magic Niu on 15/7/11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNAssetPageViewController : UIPageViewController
+
+@property (nonatomic, assign) NSInteger pageIndex;
+@property (nonatomic, assign) NSArray *assets;
+
+@end
---
+++ MNImagePicker/MNAssetPageViewController.m
@@ -0,0 +1,22 @@
+//
+//  MNAssetPageViewController.m
+//  MNImagePicker
+//
+//  Created by Magic Niu on 15/7/11.
+//
+//
+
+#import "MNAssetPageViewController.h"
+
+@interface MNAssetPageViewController ()
+
+@end
+
+@implementation MNAssetPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
